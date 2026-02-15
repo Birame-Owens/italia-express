79c3ee5a1e062a2d525056cfdaf1c433389e98f9
Birame Owens <birameowens29@gmail.com>
2026-02-15 12:50:42 +0000

    Remove unused useState import from Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { FaPhone, FaMapMarkerAlt, FaFacebook, FaInstagram, FaClock, FaCheckCircle } from 'react-icons/fa';
 import { restaurantInfo } from '../data/menuData';
